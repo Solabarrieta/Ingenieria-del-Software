19f1db190b21a99be4dd4f39ba366ace5fb64939
samuel <samuelguzmanajala@gamail.com>
2021-02-19 11:50:22 +0100

    duplicados eliminados

--- 1Marron2020/src/presentation/OfferBooking.java
+++ 1Marron2020/src/presentation/OfferBooking.java
@@ -49,42 +49,34 @@ public class OfferBooking extends JFrame {
 	private DefaultComboBoxModel<String> monthNames = new DefaultComboBoxModel<String>();
 	private JTextField year;
 
-	private JRadioButton tripleRooms;
-	private JRadioButton doubleRooms;
-	private JRadioButton singleRooms;
 
-	private ButtonGroup fareButtonGroup = new ButtonGroup();
 
-	private JButton lookforOffers1;
-	private DefaultListModel<Offer> offerInfo = new DefaultListModel<Offer>();
+
+
+
 	private DefaultComboBoxModel<Offer> offerCInfo = new DefaultComboBoxModel<Offer>();
 
-	private JList<Offer> offerList = null;
-	private JButton bookOffer = null;
 
-	private OfferManager businessLogic;
 
-	private Collection<Offer> offerCollection;
 
-	private Offer selectedOffer;
 	private JComboBox<Offer> offerCList;
-	
+
 	private JRadioButton tripleRooms;
 	private JRadioButton doubleRooms;
 	private JRadioButton singleRooms;
-	
+
 	private ButtonGroup fareButtonGroup = new ButtonGroup();  
-	
+
 	private JButton lookforOffers;
 	private DefaultListModel<Offer> offerInfo = new DefaultListModel<Offer>();
 
 	private JList<Offer> offerList = null;
 	private JButton bookOffer = null;
-	
+
 	private OfferManager businessLogic;  
 
 	private Collection<Offer> offerCollection;
-	
+
 	private Offer selectedOffer;
 
 
@@ -108,32 +100,21 @@ public class OfferBooking extends JFrame {
 
 
 	// Custom operations
-	public void setBusinessLogic(OfferManager g) {
-		businessLogic = g;
-	}
 
-	private Date newDate(int year, int month, int day) {
+
+
+	//Custom operations
+	public void setBusinessLogic(OfferManager g) {businessLogic = g;}
+
+	private Date newDate(int year,int month,int day) {
 
 		Calendar calendar = Calendar.getInstance();
-		calendar.set(year, month, day, 0, 0, 0);
+		calendar.set(year, month, day,0,0,0);
 		calendar.set(Calendar.MILLISECOND, 0);
 
 		return calendar.getTime();
 	}
 
-	
-	//Custom operations
-	public void setBusinessLogic(OfferManager g) {businessLogic = g;}
-	
-	private Date newDate(int year,int month,int day) {
-
-	     Calendar calendar = Calendar.getInstance();
-	     calendar.set(year, month, day,0,0,0);
-	     calendar.set(Calendar.MILLISECOND, 0);
-
-	     return calendar.getTime();
-	}
-	
 	/**
 	 * Create the frame.
 	 */
@@ -150,11 +131,11 @@ public class OfferBooking extends JFrame {
 		lblCity.setBounds(21, 22, 61, 16);
 		contentPane.add(lblCity);
 
-		
+
 		lblCity = new JLabel("City:");
 		lblCity.setBounds(21, 22, 61, 16);
 		contentPane.add(lblCity);
-		
+
 		city = new JTextField();
 		city.setText("Donostia");
 		city.setBounds(64, 17, 243, 26);
@@ -169,15 +150,15 @@ public class OfferBooking extends JFrame {
 		lblMonth.setBounds(117, 55, 50, 16);
 		contentPane.add(lblMonth);
 
-		
+
 		lblYear = new JLabel("Year:");
 		lblYear.setBounds(21, 55, 33, 16);
 		contentPane.add(lblYear);
-		
+
 		lblMonth = new JLabel("Month:");
 		lblMonth.setBounds(117, 55, 50, 16);
 		contentPane.add(lblMonth);
-	    
+
 		month = new JComboBox<String>();
 		month.setBounds(163, 51, 116, 27);
 		contentPane.add(month);
@@ -201,11 +182,11 @@ public class OfferBooking extends JFrame {
 		lblDay.setBounds(291, 55, 38, 16);
 		contentPane.add(lblDay);
 
-		
+
 		lblDay = new JLabel("Day:");
 		lblDay.setBounds(291, 55, 38, 16);
 		contentPane.add(lblDay);
-		
+
 		day = new JTextField();
 		day.setText("20");
 		day.setBounds(331, 50, 50, 26);
@@ -216,26 +197,26 @@ public class OfferBooking extends JFrame {
 		lblRoomType.setBounds(21, 83, 84, 16);
 		contentPane.add(lblRoomType);
 
-		
+
 		lblRoomType = new JLabel("Room Type:");
 		lblRoomType.setBounds(21, 83, 84, 16);
 		contentPane.add(lblRoomType);
-		
-		
-		
+
+
+
 		tripleRooms = new JRadioButton("Triple");
 		tripleRooms.setSelected(true);
 		fareButtonGroup.add(tripleRooms);
 		tripleRooms.setBounds(99, 79, 68, 23);
 		contentPane.add(tripleRooms);
 
-		
+
 		doubleRooms = new JRadioButton("Double");
 		fareButtonGroup.add(doubleRooms);
 		doubleRooms.setBounds(173, 79, 77, 23);
 		contentPane.add(doubleRooms);
 
-		
+
 		singleRooms = new JRadioButton("Single");
 		fareButtonGroup.add(singleRooms);
 		singleRooms.setBounds(265, 79, 77, 23);
@@ -244,172 +225,174 @@ public class OfferBooking extends JFrame {
 		lookforOffers = new JButton("Look for Concrete Offers");
 		lookforOffers.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-//				int year=;
-//				int month;
-//				int day;
-				
-				
-		
-		lookforOffers = new JButton("Look for Concrete Offers");
-		lookforOffers.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bookOffer.setEnabled(true);
-				offerInfo.clear();
-				bookOffer.setText("");
-				
-				java.util.Date date = newDate(Integer.parseInt(year.getText()), month.getSelectedIndex(),
-						Integer.parseInt(day.getText()));
-				offerCollection = businessLogic.getConcreteOffers(city.getText(), date);
-				
+				//				int year=;
+				//				int month;
+				//				int day;
 
-				
-				for (Offer v : offerCollection)
-					offerCInfo.addElement(v);
-				if (offerCollection.isEmpty())
-					searchResult.setText("No offers in that city in that date");
-				else
-					searchResult.setText("Choose an available offer in this list:");
-			}
-		});
-		lookforOffers.setBounds(91, 114, 261, 40);
-		contentPane.add(lookforOffers);
 
-		searchResult = new JLabel("");
-		searchResult.setBounds(109, 180, 243, 16);
-		contentPane.add(searchResult);
 
-		bookOffer = new JButton("");
-		bookOffer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				int num = 0;
-				boolean error = false;
-				if (tripleRooms.isSelected()) {
-					num = selectedOffer.getTripleNumber();
-					selectedOffer.setTripleNumber(num - 1);
-					
-				} else if (doubleRooms.isSelected()) {
-					num = selectedOffer.getDoubleNumber();
-					selectedOffer.setDoubleNumber(num - 1);
-					
-				} else if (singleRooms.isSelected()) {
-					num = selectedOffer.getSingleNumber();
-					selectedOffer.setSingleNumber(num - 1);
-				}
-				if (error)
-					bookOffer.setText("Error: There were no offers available!");
-				else
-					bookOffer.setText("Booked. #rooms left: " + (num - 1));
-				java.util.Date date =newDate(Integer.parseInt(year.getText()),month.getSelectedIndex(),Integer.parseInt(day.getText()));
-				 
-				offerCollection=businessLogic.getConcreteOffers(city.getText(),date);
-				for (Offer v : offerCollection)  offerInfo.addElement(v); 
-				if (offerCollection.isEmpty()) searchResult.setText("No offers in that city in that date");
-				else searchResult.setText("Choose an available offer in this list:");
-			}
-		});
-		lookforOffers.setBounds(91, 114, 261, 40);
-		contentPane.add(lookforOffers);	
-		
-		searchResult = new JLabel("");
-		searchResult.setBounds(109, 180, 243, 16);
-		contentPane.add(searchResult);
-		
-		offerList = new JList<Offer>();
-		offerList.setBounds(64, 199, 336, 71);
-		contentPane.add(offerList);
-		offerList.setModel(offerInfo);
-		offerList.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
-			public void valueChanged(javax.swing.event.ListSelectionEvent e) {
-				if (e.getValueIsAdjusting()) return; // El evento se dispara dos veces: antes de cambiar el valor y una vez cambiado
-													 // Interesa s�lo actuar una vez cambiado
-				if (!offerList.isSelectionEmpty()){ // A este m�todo se le llama tambi�n cuando se hace un clear del JList, 
-													 // por lo que podr�a estar la selecci�n vac�a y dar un error
-					selectedOffer = (Offer) offerList.getSelectedValue();
-					bookOffer.setEnabled(true);
-					bookOffer.setText("Book this offer: "+selectedOffer);  // TODO Auto-generated Event stub valueChanged()
-				}
-			}
-		});
-		
-		bookOffer = new JButton("");
-		bookOffer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				int num=0;
-				boolean error=false;
-				if (tripleRooms.isSelected()) { 
-					num=selectedOffer.getTripleNumber();
-					if (num>0) selectedOffer.setTripleNumber(num-1); else error=true; 
-				}
-				else if (doubleRooms.isSelected()) {
-					num=selectedOffer.getDoubleNumber();
-					if (num>0) selectedOffer.setDoubleNumber(num-1); else error=true;
-				}
-				else if (singleRooms.isSelected()) {
-					num=selectedOffer.getSingleNumber();
-					if (num>0) selectedOffer.setSingleNumber(num-1); else error=true;
-				}
-				if (error) bookOffer.setText("Error: There were no offers available!");
-				else bookOffer.setText("Booked. #rooms left: "+(num-1));
-				bookOffer.setEnabled(false);
-			}
-		});
-		bookOffer.setBounds(21, 282, 399, 40);
-		contentPane.add(bookOffer);
-
-		year = new JTextField();
-		year.setText("2020");
-		year.setBounds(57, 50, 50, 26);
-		contentPane.add(year);
-		year.setColumns(10);
-
-		offerCList = new JComboBox<Offer>();
-		offerCList.setBounds(91, 209, 261, 40);
-		contentPane.add(offerCList);
-		offerCList.setModel(offerCInfo);
-		offerCList.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		if (offerCList.getSelectedItem()!=null) { 
-			
-				selectedOffer = (Offer) offerCList.getSelectedItem();
-				int num;
-				if (tripleRooms.isSelected()) {
-					num=selectedOffer.getTripleNumber();
-					if (num<1) {
-						bookOffer.setEnabled(false);
-						bookOffer.setText("No rooms available");
-					}
-					else {
+				lookforOffers = new JButton("Look for Concrete Offers");
+				lookforOffers.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
 						bookOffer.setEnabled(true);
-						bookOffer.setText("Book this offer: " + selectedOffer); 
+						offerInfo.clear();
+						bookOffer.setText("");
+
+						java.util.Date date = newDate(Integer.parseInt(year.getText()), month.getSelectedIndex(),
+								Integer.parseInt(day.getText()));
+						offerCollection = businessLogic.getConcreteOffers(city.getText(), date);
+
+
+
+						for (Offer v : offerCollection)
+							offerCInfo.addElement(v);
+						if (offerCollection.isEmpty())
+							searchResult.setText("No offers in that city in that date");
+						else
+							searchResult.setText("Choose an available offer in this list:");
 					}
-				}
-				if (doubleRooms.isSelected()) {
-					num=selectedOffer.getDoubleNumber();
-					if (num<1) {
+				});
+				lookforOffers.setBounds(91, 114, 261, 40);
+				contentPane.add(lookforOffers);
+
+				searchResult = new JLabel("");
+				searchResult.setBounds(109, 180, 243, 16);
+				contentPane.add(searchResult);
+
+				bookOffer = new JButton("");
+				bookOffer.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						int num = 0;
+						boolean error = false;
+						if (tripleRooms.isSelected()) {
+							num = selectedOffer.getTripleNumber();
+							selectedOffer.setTripleNumber(num - 1);
+
+						} else if (doubleRooms.isSelected()) {
+							num = selectedOffer.getDoubleNumber();
+							selectedOffer.setDoubleNumber(num - 1);
+
+						} else if (singleRooms.isSelected()) {
+							num = selectedOffer.getSingleNumber();
+							selectedOffer.setSingleNumber(num - 1);
+						}
+						if (error)
+							bookOffer.setText("Error: There were no offers available!");
+						else
+							bookOffer.setText("Booked. #rooms left: " + (num - 1));
+						java.util.Date date =newDate(Integer.parseInt(year.getText()),month.getSelectedIndex(),Integer.parseInt(day.getText()));
+
+						offerCollection=businessLogic.getConcreteOffers(city.getText(),date);
+						for (Offer v : offerCollection)  offerInfo.addElement(v); 
+						if (offerCollection.isEmpty()) searchResult.setText("No offers in that city in that date");
+						else searchResult.setText("Choose an available offer in this list:");
+					}
+				});
+				lookforOffers.setBounds(91, 114, 261, 40);
+				contentPane.add(lookforOffers);	
+
+				searchResult = new JLabel("");
+				searchResult.setBounds(109, 180, 243, 16);
+				contentPane.add(searchResult);
+
+				offerList = new JList<Offer>();
+				offerList.setBounds(64, 199, 336, 71);
+				contentPane.add(offerList);
+				offerList.setModel(offerInfo);
+				offerList.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+					public void valueChanged(javax.swing.event.ListSelectionEvent e) {
+						if (e.getValueIsAdjusting()) return; // El evento se dispara dos veces: antes de cambiar el valor y una vez cambiado
+						// Interesa s�lo actuar una vez cambiado
+						if (!offerList.isSelectionEmpty()){ // A este m�todo se le llama tambi�n cuando se hace un clear del JList, 
+							// por lo que podr�a estar la selecci�n vac�a y dar un error
+							selectedOffer = (Offer) offerList.getSelectedValue();
+							bookOffer.setEnabled(true);
+							bookOffer.setText("Book this offer: "+selectedOffer);  // TODO Auto-generated Event stub valueChanged()
+						}
+					}
+				});
+
+				bookOffer = new JButton("");
+				bookOffer.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						int num=0;
+						boolean error=false;
+						if (tripleRooms.isSelected()) { 
+							num=selectedOffer.getTripleNumber();
+							if (num>0) selectedOffer.setTripleNumber(num-1); else error=true; 
+						}
+						else if (doubleRooms.isSelected()) {
+							num=selectedOffer.getDoubleNumber();
+							if (num>0) selectedOffer.setDoubleNumber(num-1); else error=true;
+						}
+						else if (singleRooms.isSelected()) {
+							num=selectedOffer.getSingleNumber();
+							if (num>0) selectedOffer.setSingleNumber(num-1); else error=true;
+						}
+						if (error) bookOffer.setText("Error: There were no offers available!");
+						else bookOffer.setText("Booked. #rooms left: "+(num-1));
 						bookOffer.setEnabled(false);
-						bookOffer.setText("No rooms available");
 					}
-					else {
-						bookOffer.setEnabled(true);
-						bookOffer.setText("Book this offer: " + selectedOffer); 
+				});
+				bookOffer.setBounds(21, 282, 399, 40);
+				contentPane.add(bookOffer);
+
+				year = new JTextField();
+				year.setText("2020");
+				year.setBounds(57, 50, 50, 26);
+				contentPane.add(year);
+				year.setColumns(10);
+
+				offerCList = new JComboBox<Offer>();
+				offerCList.setBounds(91, 209, 261, 40);
+				contentPane.add(offerCList);
+				offerCList.setModel(offerCInfo);
+				offerCList.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if (offerCList.getSelectedItem()!=null) { 
+
+							selectedOffer = (Offer) offerCList.getSelectedItem();
+							int num;
+							if (tripleRooms.isSelected()) {
+								num=selectedOffer.getTripleNumber();
+								if (num<1) {
+									bookOffer.setEnabled(false);
+									bookOffer.setText("No rooms available");
+								}
+								else {
+									bookOffer.setEnabled(true);
+									bookOffer.setText("Book this offer: " + selectedOffer); 
+								}
+							}
+							if (doubleRooms.isSelected()) {
+								num=selectedOffer.getDoubleNumber();
+								if (num<1) {
+									bookOffer.setEnabled(false);
+									bookOffer.setText("No rooms available");
+								}
+								else {
+									bookOffer.setEnabled(true);
+									bookOffer.setText("Book this offer: " + selectedOffer); 
+								}
+							}
+							if (singleRooms.isSelected()) {
+								num=selectedOffer.getSingleNumber();
+								if (num<1) {
+									bookOffer.setEnabled(false);
+									bookOffer.setText("No rooms available");
+								}
+								else {
+									bookOffer.setEnabled(true);
+									bookOffer.setText("Book this offer: " + selectedOffer); 
+								}
+							}
+						}
+
 					}
-				}
-				if (singleRooms.isSelected()) {
-					num=selectedOffer.getSingleNumber();
-					if (num<1) {
-						bookOffer.setEnabled(false);
-						bookOffer.setText("No rooms available");
-					}
-					else {
-						bookOffer.setEnabled(true);
-						bookOffer.setText("Book this offer: " + selectedOffer); 
-					}
-				}
-			}
+
+				});
 
 			}
-
 		});
-
 	}
 }
